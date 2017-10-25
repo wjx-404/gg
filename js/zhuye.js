1084b6bd9fd626a90a92b39119558808e3f8630d
发飙的蜗牛 <1103423621@qq.com>
2017-10-25 21:25:58 +0800

    x.1.1

---
+++ js/zhuye.js
@@ -0,0 +1,250 @@
+$(function() {
+    $('#index-nav .y').on('click', function() {
+        $(".wjx-sy").attr("style", "display:block");
+        $("#wjx-sc").attr("style", "display:none");
+        $(".wjx-xxk").attr("style", "display:none");
+        $("#wjx-ks").attr("style", "display:none");
+        $(".wjx-bj,.wjx-gj,.wjx-ck,.wjx-cy,.wjx-en").attr("style", "display:noe");
+    })
+});
+$(function() {
+    $('#index-nav .j').on('click', function() {
+        $(".wjx-bj").attr("style", "display:block");
+        $("#wjx-sc").attr("style", "display:none");
+        $(".wjx-xxk").attr("style", "display:none");
+        $("#wjx-ks").attr("style", "display:none");
+        $(".wjx-sy,.wjx-gj,.wjx-ck,.wjx-cy,.wjx-en").attr("style", "display:noe");
+    })
+});
+$(function() {
+    $('#index-nav .g').on('click', function() {
+        $(".wjx-gj").attr("style", "display:block");
+        $("#wjx-sc").attr("style", "display:none");
+        $(".wjx-xxk").attr("style", "display:none");
+        $("#wjx-ks").attr("style", "display:none");
+        $(".wjx-sy,.wjx-bj,.wjx-ck,.wjx-cy,.wjx-en").attr("style", "display:noe");
+    })
+});
+$(function() {
+    $('#index-nav .c').on('click', function() {
+        $(".wjx-ck").attr("style", "display:block");
+        $(".wjx-xxk").attr("style", "display:block");
+        $("#wjx-sc").attr("style", "display:block");
+        $(".wjx-sy,.wjx-gj,.wjx-bj,.wjx-cy,.wjx-en").attr("style", "display:noe");
+    })
+});
+$(function() {
+    $('#index-nav .s').on('click', function() {
+        $(".wjx-cy").attr("style", "display:block");
+        $("#wjx-sc").attr("style", "display:none");
+        $(".wjx-xxk").attr("style", "display:block");
+        $("#wjx-ks").attr("style", "display:block");
+        $(".wjx-sy,.wjx-gj,.wjx-ck,.wjx-bj,.wjx-en").attr("style", "display:noe");
+    })
+});
+$(function() {
+    $('#index-nav .entera').on('click', function() {
+        $(".wjx-en").attr("style", "display:block");
+        $("#wjx-sc").attr("style", "display:none");
+        $(".wjx-xxk").attr("style", "display:none");
+        $("#wjx-ks").attr("style", "display:none");
+        $(".wjx-sy,.wjx-gj,.wjx-ck,.wjx-cy,.wjx.bj").attr("style", "display:noe");
+    })
+});
+
+$(function() {
+    $('.left .tab').on('click', function() {
+        $(".right .cate1").attr("style", "display:block");
+        $(".right .cate2").attr("style", "display:block");
+        $(".right .cate3").attr("style", "display:block");
+        $(".right .cate4").attr("style", "display:block");
+        $(".right .cate5").attr("style", "display:block");
+        $(".right .cate6").attr("style", "display:block");
+        $(".right .cate7").attr("style", "display:block");
+        $(".right .cate8").attr("style", "display:block");
+        $(".right .cate9").attr("style", "display:block");
+        $(".right .cate10").attr("style", "display:block");
+    })
+});
+
+$(function() {
+    $('.left #cat1').on('click', function() {
+        $(".right .cate1").attr("style", "display:block");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat2').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:block");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat3').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:block");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat4').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:block");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat5').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:block");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat6').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:block");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat7').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:block");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat8').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:block");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat9').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:block");
+        $(".right .cate10").attr("style", "display:none");
+    })
+});
+
+$(function() {
+    $('.left #cat10').on('click', function() {
+        $(".right .cate1").attr("style", "display:none");
+        $(".right .cate2").attr("style", "display:none");
+        $(".right .cate3").attr("style", "display:none");
+        $(".right .cate4").attr("style", "display:none");
+        $(".right .cate5").attr("style", "display:none");
+        $(".right .cate6").attr("style", "display:none");
+        $(".right .cate7").attr("style", "display:none");
+        $(".right .cate8").attr("style", "display:none");
+        $(".right .cate9").attr("style", "display:none");
+        $(".right .cate10").attr("style", "display:block");
+    })
+});
+
+$(function() {
+    $(".fixed-btn .qrcode").mouseover(function() {
+        $(".hove").attr("style", "display:block");
+    });
+    $(".fixed-btn .qrcode").mouseout(function() {
+        $(".hove").attr("style", "display:none");
+    });
+});
+
+
+$(function() {
+    $('.entera .modal-content .pull-right').on('click', function() {
+        $(".den").attr("style", "display:none");
+        $(".zhuce").attr("style", "display:block");
+        $(".zhuc").css("background-color", "white");
+        $(".zhuc").css("color", "#333434");
+        $(".zh").css("color", "#9FBE8A");
+        $(".zh").css("background-color", "#E8F1E2");
+    })
+});
+
+$(function() {
+    $('.entera .modal-content .pull-left').on('click', function() {
+        $(".den").attr("style", "display:block");
+        $(".zhuce").attr("style", "display:none");
+        $(".zhuc").css("background-color", "#E8F1E2");
+        $(".zhuc").css("color", "#9FBE8A");
+        $(".zh").css("color", "#333434");
+        $(".zh").css("background-color", "white");
+    })
+});
